d1141c49aa72d06377ff918776281e18716396b2
‪Karim Negm‬‏ <60659601+Kareem-negm@users.noreply.github.com>
2021-01-14 21:33:45 +0200

    Update kidne-app.py

--- kidne-app.py
+++ kidne-app.py
@@ -22,7 +22,7 @@ st.write("""
 df = pd.read_csv("https://github.com/Kareem-negm/Chronic_Kidney_Disease_prediction/blob/main/Chronic_KIdney_Disease_data.csv")
 #Show the data as a table (you can also use st.write(df))
 
-st.image("https://github.com/Kareem-negm/Chronic_Kidney_Disease_prediction/blob/main/images/img.jpg")
+#st.image("https://github.com/Kareem-negm/Chronic_Kidney_Disease_prediction/blob/main/images/img.jpg")
 
 st.subheader('Data Information:')
 
